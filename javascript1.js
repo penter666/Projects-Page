9e7220c197b373b4db87248372927fdd7f602d56
pentirecords <55096308+pentirecords@users.noreply.github.com>
2019-09-09 21:01:04 +0300

    some change

---
+++ javascript1.js
@@ -0,0 +1,14 @@
+
+
+var disqus_config = function () {
+this.page.url = 'https://pentirecords.github.io/Penti/index2.html';
+this.page.identifier = PAGE_IDENTIFIER;
+};
+
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://pentirecords.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
